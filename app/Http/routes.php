0ab86ab1770cafa5538ed4747032c50112dc531b
mokh21 <mokh.benaissia@gmail.com>
2016-03-29 15:51:27 +0100

    2nd commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,36 +26,33 @@ Route::get('produit/{id}', 'ProduitsController@show');
 Route::post('modif/{id}', 'ProduitsController@update');
 Route::get('effacer/{id}', 'ProduitsController@destroy');
 */
+   Route::get('/home', 'HomeController@index');
    
 
-Route::group(['middleware' => ['web']], function () {
+   Route::group(['middleware' => ['web']], function () {
+ Route::auth();
+
+
+
+});
+
+Route::group(['middleware' => ['web', 'auth']], function () {
     Route::resource('produits', 'ProduitsController');    
     Route::resource('utilisateurs', 'UserController');
     Route::resource('client', 'ClientController');
     Route::resource('rdv', 'RdvController');
-    Route::auth();
+    
     Route::get('daterdv', 'RdvController@showdaterdv');
     Route::resource('admins', 'AdminsController');
     Route::resource('mail', 'MailController');
     Route::resource('commandes', 'CommandesController');
     Route::resource('product', 'ProductController');
     Route::resource('residence', 'ResidenceController');
-Route::get('/home', 'HomeController@index');
+    
 
 
 
 
-
-/*
-// Authentication routes..
-Route::get('auth/login', 'Auth\AuthController@getLogin');
-Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
-Route::post('auth/register', 'Auth\AuthController@create');
-*/
 });
     
 
